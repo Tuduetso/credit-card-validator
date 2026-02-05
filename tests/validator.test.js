ef8a79d0bc93f83b8b91627db27facce10377459
Tuduetso <tudumpe@gmail.com>
2026-02-05 17:00:58 +0200

    Fix my test expectation

--- tests/validator.test.js
+++ tests/validator.test.js
@@ -1,4 +1,4 @@
-const { isAValidCreditCard } = require('..src/validator');
+const { isAValidCreditCard } = require('../src/validator');
 
 test('49927398716 is valid', () => {
   expect(isAValidCreditCard('49927398716')).toBe(true);
@@ -12,11 +12,11 @@ test('234567812345678 is invalid', () => {
   expect(isAValidCreditCard('234567812345678')).toBe(false);
 });
 test('1234567812345670 is valid', () => {
-  expect(isAValidCreditCard('21234567812345670')).toBe(false);
+  expect(isAValidCreditCard('121234567812345670')).toBe(true);
 });
 test('2222405343248877 is valid', () => {
-  expect(isAValidCreditCard('2222405343248877')).toBe(false);
+  expect(isAValidCreditCard('2222405343248877')).toBe(true);
 });
 test('2222990905257051 is valid', () => {
-  expect(isAValidCreditCard('2222990905257051')).toBe(false);
+  expect(isAValidCreditCard('2222990905257051')).toBe(true);
 });
