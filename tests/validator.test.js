1462086371c6c05229e086171a34959fb0c636ba
Tuduetso <tudumpe@gmail.com>
2026-02-05 16:37:43 +0200

    Fix jest setup

---
+++ tests/validator.test.js
@@ -0,0 +1,22 @@
+const { isAValidCreditCard } = require('./validator');
+
+test('49927398716 is valid', () => {
+  expect(isAValidCreditCard('49927398716')).toBe(true);
+});
+
+test('49927398717 is invalid', () => {
+  expect(isAValidCreditCard('49927398717')).toBe(false);
+});
+
+test('234567812345678 is invalid', () => {
+  expect(isAValidCreditCard('234567812345678')).toBe(false);
+});
+test('1234567812345670 is valid', () => {
+  expect(isAValidCreditCard('21234567812345670')).toBe(false);
+});
+test('2222405343248877 is valid', () => {
+  expect(isAValidCreditCard('2222405343248877')).toBe(false);
+});
+test('2222990905257051 is valid', () => {
+  expect(isAValidCreditCard('2222990905257051')).toBe(false);
+});
